1ebb4c3e6bf5282202bbda223fb13a31a6658f5a
Ben Cromwell <ben@bencromwell.com>
2021-08-16 13:43:58 +0100

    Fix autoloader paths (differ when composer installed)

--- bin/git-spruce.php
+++ bin/git-spruce.php
@@ -1,6 +1,10 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    require_once __DIR__ . '/../vendor/autoload.php';
+} elseif (file_exists(__DIR__ . '/../../../autoload.php')) {
+    require_once __DIR__ . '/../../../autoload.php';
+}
 
 use Cromwell\GitSpruce\GitSpruce;
 use Cromwell\GitSpruce\Command\CleanBranchesCommand;
